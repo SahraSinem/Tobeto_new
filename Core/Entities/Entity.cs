38baa4792a48e33947e2727f521e3e0db846f5b5
SahraSinem <s.sinem_can@hotmail.com>
2023-12-07 01:59:07 +0300

    DataAccess Core katmanları eklendi

--- Core/Entities/Entity.cs
+++ Core/Entities/Entity.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    
-        public class Entity<TId> // : IEntityTimestamps
+
+    public class Entity<TId> : IEntityTimestamps
+    {
+        public TId Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+        public Entity()
         {
-            public TId Id { get; set; }
-            public DateTime CreatedDate { get; set; }
-            public DateTime? UpdatedDate { get; set; }
-            public DateTime? DeletedDate { get; set; }
-
-            public Entity()
-            {
-                Id = default;
-            }
-
-            public Entity(TId id)
-            {
-                Id = id;
-            }
+            Id = default;
         }
-    
+
+        public Entity(TId id)
+        {
+            Id = id;
+        }
+    }
+
 }
---
+++ DataAccess/Abstracts/IProductDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstracts
+{
+    public interface IProductDal // : IRepository<Product, int>, IAsyncRepository<Product, int> 
+    {
+    }
+}
