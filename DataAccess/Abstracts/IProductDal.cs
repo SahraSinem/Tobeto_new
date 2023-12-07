5d7aff2ddba6e1ac8dd351617106bfa00f78c1f0
SahraSinem <s.sinem_can@hotmail.com>
2023-12-08 00:38:31 +0300

    Product Katmanları Oluşturuldu

--- DataAccess/Abstracts/IProductDal.cs
+++ DataAccess/Abstracts/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.DataAccess.Repository;
+using Entities.Concretes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstracts
 {
-    public interface IProductDal // : IRepository<Product, int>, IAsyncRepository<Product, int> 
+    public interface IProductDal : IRepository<Product, int>, IAsyncRepository<Product, int> 
     {
+
+
     }
 }
